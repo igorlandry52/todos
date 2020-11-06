6542deb36547eec503c7dfa2df460698233c2aed
igor <igorlandry52@yahoo.fr>
2020-11-06 19:11:43 +0100

    commit

--- todo/src/main.js
+++ todo/src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Bootstrap from "bootstrap";
+import BootstrapVue from "bootstrap-vue";
 
 Vue.config.productionTip = false;
+Vue.use(Bootstrap);
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
